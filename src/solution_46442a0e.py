74e4681ae9dc6d9a4ae08d01ac86ddb78c1245d8
BarryMonahan <58415572+BarryMonahan@users.noreply.github.com>
2019-12-02 07:22:00 +0000

    Load training data from json rather than hard-coded

--- src/solution_46442a0e.py
+++ src/solution_46442a0e.py
@@ -1,7 +1,13 @@
 from src import utils
-
+import sys
+import os
 
 def solve(in_array):
+    '''
+    Given the input set, extend the size twice in both directions, with each new quadrant being a rotation of the input
+    :param in_array: input array
+    :return: expected output array
+    '''
     array_edgelength = len(in_array[0])   # input array edge length
     opp_end = array_edgelength*2-1    # used for getting opposite end of array
     prediction = [[-1]*array_edgelength*2 for i in range(array_edgelength*2)]  # init 2d array
@@ -20,6 +26,6 @@ def solve(in_array):
     return prediction
 
 
-training_1 = {"train": [{"input": [[8, 6], [6, 8]], "output": [[8, 6, 6, 8], [6, 8, 8, 6], [6, 8, 8, 6], [8, 6, 6, 8]]}, {"input": [[7, 7, 8], [7, 7, 8], [8, 8, 8]], "output": [[7, 7, 8, 8, 7, 7], [7, 7, 8, 8, 7, 7], [8, 8, 8, 8, 8, 8], [8, 8, 8, 8, 8, 8], [7, 7, 8, 8, 7, 7], [7, 7, 8, 8, 7, 7]]}, {"input": [[6, 9, 9], [6, 4, 4], [6, 4, 4]], "output": [[6, 9, 9, 6, 6, 6], [6, 4, 4, 4, 4, 9], [6, 4, 4, 4, 4, 9], [9, 4, 4, 4, 4, 6], [9, 4, 4, 4, 4, 6], [6, 6, 6, 9, 9, 6]]}], "test": [{"input": [[1, 4, 1], [4, 9, 4], [9, 1, 9]], "output": [[1, 4, 1, 9, 4, 1], [4, 9, 4, 1, 9, 4], [9, 1, 9, 9, 4, 1], [1, 4, 9, 9, 1, 9], [4, 9, 1, 4, 9, 4], [1, 4, 9, 1, 4, 1]]}]}
-
-utils.run_tests(training_1, solve)
+if __name__ == "__main__":
+    training_data = utils.load_json_from_command_arg()
+    utils.run_tests(training_data, solve)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,21 @@
+import numpy as np
+import sys
+import os
+import json
+
+
+def load_json_from_command_arg():
+    if len(sys.argv) < 2:
+        print("ERROR: script requires the path to the training data as an argument - eg: data/training/46442a0e.json")
+        sys.exit(-1)
+    training_data_path = sys.argv[1]
+    if not os.path.isfile(training_data_path):
+        print("ERROR: training data argument is not an existing file.")
+        sys.exit(-2)
+
+    with open(training_data_path) as json_file:
+        return json.load(json_file)
+
 def run_tests(training_set, solving_function):
     training_set['train'].extend(training_set['test'])
     for case in training_set['train']:
@@ -7,6 +25,10 @@ def run_tests(training_set, solving_function):
         prediction = solving_function(input)
 
         if prediction == output:
-            print("Test passed.")
+            print("INPUT:")
+            print(np.matrix(input))
+            print("OUTPUT/SOLUTION:")
+            print(np.matrix(prediction))
+            print("Test passed.\n")
         else:
             print("ERROR: TEST FAILED")
