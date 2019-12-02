4ae4f719a1000d0529f787a43055abaf26314b6d
BarryMonahan <58415572+BarryMonahan@users.noreply.github.com>
2019-12-02 07:03:50 +0000

    Initial solution

---
+++ src/solution_46442a0e.py
@@ -0,0 +1,32 @@
+def solve(in_array):
+    array_edgelength = len(in_array[0])   # input array edge length
+    opp_end = array_edgelength*2-1    # used for getting opposite end of array
+    prediction = [[-1]*array_edgelength*2 for i in range(array_edgelength*2)]  # init 2d array
+
+    # iterate through all values
+    for y in range(len(in_array)):
+        for x in range(len(in_array[0])):
+            val = in_array[y][x]
+
+            prediction[y][x] = val
+            # other 3 quadrants are rotated
+            prediction[x][opp_end-y] = val
+            prediction[opp_end-y][opp_end-x] = val
+            prediction[opp_end-x][y] = val
+
+    return prediction
+
+
+training_1 = {"train": [{"input": [[8, 6], [6, 8]], "output": [[8, 6, 6, 8], [6, 8, 8, 6], [6, 8, 8, 6], [8, 6, 6, 8]]}, {"input": [[7, 7, 8], [7, 7, 8], [8, 8, 8]], "output": [[7, 7, 8, 8, 7, 7], [7, 7, 8, 8, 7, 7], [8, 8, 8, 8, 8, 8], [8, 8, 8, 8, 8, 8], [7, 7, 8, 8, 7, 7], [7, 7, 8, 8, 7, 7]]}, {"input": [[6, 9, 9], [6, 4, 4], [6, 4, 4]], "output": [[6, 9, 9, 6, 6, 6], [6, 4, 4, 4, 4, 9], [6, 4, 4, 4, 4, 9], [9, 4, 4, 4, 4, 6], [9, 4, 4, 4, 4, 6], [6, 6, 6, 9, 9, 6]]}], "test": [{"input": [[1, 4, 1], [4, 9, 4], [9, 1, 9]], "output": [[1, 4, 1, 9, 4, 1], [4, 9, 4, 1, 9, 4], [9, 1, 9, 9, 4, 1], [1, 4, 9, 9, 1, 9], [4, 9, 1, 4, 9, 4], [1, 4, 9, 1, 4, 1]]}]}
+
+training_1['train'].extend(training_1['test'])  # for this assignment, combine testing and training data together
+for case in training_1['train']:
+    input = case['input']
+    output = case['output']
+
+    prediction = solve(input)
+
+    if prediction == output:
+        print("Test passed.")
+    else:
+        print("ERROR: TEST FAILED")
